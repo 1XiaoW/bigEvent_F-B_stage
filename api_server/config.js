1a006fb3962f624060b8dbaaf72252ddf7277f4a
linxiaowan <1604808220@qq.com>
2023-01-30 22:17:10 +0800

    初次提交，仅完成视频所有内容

---
+++ api_server/config.js
@@ -0,0 +1,8 @@
+//这是一个全局的配置文件
+
+module.exports = {
+  //加密和解密的Token的密钥
+  jwtSecretKey: 'lxw No1 ^_^',
+  //这是token的有效期
+  expiresIn: '10h',
+};
---
+++ api_server/router/article.js
@@ -0,0 +1,33 @@
+//文章的路由模块
+const express = require('express');
+const router = express.Router();
+
+//导入需要的处理函数模块
+const article_handler = require('../router_handler/article');
+
+//导入multer和path
+const multer = require('multer');
+const path = require('path');
+
+//创建multer的实例
+const uploads = multer({ dest: path.join(__dirname, '../uploads') });
+
+//导入校验模块和校验规则
+const expressJoi = require('@escook/express-joi');
+const { add_article_schema } = require('../schema/article');
+
+//发布文章的路由
+router.post(
+  '/add',
+  uploads.single('cover_img'),
+  expressJoi(add_article_schema),
+  article_handler.addArticle
+);
+
+/*TODO：
+1.获取文章的列表数据
+2.根据Id删除文章数据
+3.根据Id获取文章详情
+4.根据Id更新文章信息
+*/
+module.exports = router;
