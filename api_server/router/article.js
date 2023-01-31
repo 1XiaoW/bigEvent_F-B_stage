3bb2a78182a97aa4613d251a2fc8655036b9bb32
linxiaowan <1604808220@qq.com>
2023-01-31 17:48:41 +0800

    已完成新增接口的数据验证

--- api_server/router/article.js
+++ api_server/router/article.js
@@ -14,7 +14,12 @@ const uploads = multer({ dest: path.join(__dirname, '../uploads') });
 
 //导入校验模块和校验规则
 const expressJoi = require('@escook/express-joi');
-const { add_article_schema } = require('../schema/article');
+const {
+  add_article_schema,
+  delete_get_article_schema,
+  getList_article_schema,
+  update_article_schema,
+} = require('../schema/article');
 
 //发布文章的路由
 router.post(
@@ -25,18 +30,31 @@ router.post(
 );
 
 //获取文章的列表数据
-router.get('/list', article_handler.getArticle);
+router.get(
+  '/list',
+  expressJoi(getList_article_schema),
+  article_handler.getArticle
+);
 
 //根据Id删除文章数据
-router.get('/delete/:id', article_handler.deleteArticleById);
+router.get(
+  '/delete/:id',
+  expressJoi(delete_get_article_schema),
+  article_handler.deleteArticleById
+);
 
 //根据Id获取文章详情
-router.get('/:id', article_handler.getArticleById);
+router.get(
+  '/:id',
+  expressJoi(delete_get_article_schema),
+  article_handler.getArticleById
+);
 
 //根据Id更新文章信息
 router.post(
   '/edit',
   uploads.single('cover_img'),
+  expressJoi(update_article_schema),
   article_handler.updateArticle
 );
 
--- api_server/schema/article.js
+++ api_server/schema/article.js
@@ -3,12 +3,46 @@ const joi = require('joi');
 //分别定义标题、分类Id、内容、发布状态的校验规则
 const title = joi.string().required();
 const cate_id = joi.number().integer().min(1).required();
+const cate_id1 = joi.number().integer().min(1);
 const content = joi.string().required().allow('');
 const state = joi.string().valid('已发布', '草稿').required();
+const state1 = joi.string().valid('已发布', '草稿');
+
+const id = joi.number().integer().min(1).required();
+const pagesize = joi.number().integer().min(1).required();
+const pagenum = joi.number().integer().min(1).required();
 
 //验证规则对象-发布文章
 exports.add_article_schema = {
   body: {
+    title,
+    cate_id,
+    content,
+    state,
+  },
+};
+
+//验证规则对象-Id检测
+exports.delete_get_article_schema = {
+  params: {
+    id,
+  },
+};
+
+//验证规则对象-文章列表检测
+exports.getList_article_schema = {
+  query: {
+    state: state1,
+    cate_id: cate_id1,
+    pagesize,
+    pagenum,
+  },
+};
+
+//验证规则对象-更新文章检测
+exports.update_article_schema = {
+  body: {
+    Id: id,
     title,
     cate_id,
     content,
